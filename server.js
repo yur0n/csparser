2383c1fa5fa5bbcc06c39be12c6360982d0247ca
Yuri <yur0n@yahoo.com>
2024-03-02 18:56:36 +0200

    up

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const {csparser} = require('./csparser.js')
 const {getSteamId} = require('./getSteamId.js')
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 let codes = ['838295', '95973', '615738', '519592', '287358'];
 
 app.use(bodyParser.json());
@@ -53,8 +53,8 @@ passport.deserializeUser(function(obj, done) {
   //   credentials (in this case, an OpenID identifier and profile), and invoke a
   //   callback with a user object.
 passport.use(new SteamStrategy({
-        returnURL: 'http://localhost:8080/auth/steam/return',
-        realm: 'http://localhost:8080/',
+        returnURL: process.env.HOST + PORT + '/auth/steam/return' || 'http://localhost:8080/auth/steam/return',
+        realm: process.env.HOST + PORT || 'http://localhost:8080/',
         apiKey: process.env.STEAM_API_KEY
     },
     function(identifier, profile, done) {
